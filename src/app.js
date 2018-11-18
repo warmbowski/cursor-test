2279ea4ac940ab0774bccd32f8f4efc16ad7c095
Paul Laskowski <paul@warmbowski.org>
2018-11-17 21:04:30 -0800

    fix indentation

--- src/app.js
+++ src/app.js
@@ -3,44 +3,44 @@ import {createBinder} from 'immutable-binder';
 
 export class Item extends React.Component{
 
-  increase = () => {
-    var quantity = this.props.item.quantity.getValue();
-    this.props.item.quantity.setValue(quantity + 1);
-  }
+    increase = () => {
+        var quantity = this.props.item.quantity.getValue();
+        this.props.item.quantity.setValue(quantity + 1);
+    }
 
-  subTotal= () => {
-    return this.props.item.quantity.getValue() * this.props.item.price.getValue();
-  }
+    subTotal= () => {
+        return this.props.item.quantity.getValue() * this.props.item.price.getValue();
+    }
 
-  render() {
-    return(
-      <div className="item" style={{ width: '150px', display: 'flex', justifyContent: 'space-between' }}>
-        <a href="#" onClick={this.increase}>+</a>
-        <span>{this.props.item.quantity.getValue()}</span>
-        <span>{this.props.item.name.getValue()}</span>
-        <span>${this.subTotal()}</span>
-      </div>
-    );
-  }
-};
-
-export class Order extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = { 
-        orderBinder: createBinder(props.order, this.handleChange)
+    render() {
+        return(
+        <div className="item" style={{ width: '150px', display: 'flex', justifyContent: 'space-between' }}>
+            <a href="#" onClick={this.increase}>+</a>
+            <span>{this.props.item.quantity.getValue()}</span>
+            <span>{this.props.item.name.getValue()}</span>
+            <span>${this.subTotal()}</span>
+        </div>
+        );
+    }
     };
-  }
-  
-  handleChange = (newRootBinder) => {
-    this.setState({ orderBinder: newRootBinder });
-  }
 
-  render() {
-    var items = this.state.orderBinder.map((item, idx) => <Item key={idx} item={item} />);
+    export class Order extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = { 
+            orderBinder: createBinder(props.order, this.handleChange)
+        };
+    }
+    
+    handleChange = (newRootBinder) => {
+        this.setState({ orderBinder: newRootBinder });
+    }
 
-    return(
-      <div>{items}</div>
-    );
-  }
+    render() {
+        var items = this.state.orderBinder.map((item, idx) => <Item key={idx} item={item} />);
+
+        return(
+        <div>{items}</div>
+        );
+    }
 };
--- src/index.js
+++ src/index.js
@@ -5,24 +5,11 @@ import ReactDOM from "react-dom";
 import { Order } from "./app";
 
 var orderData = [
-  {name: "Burger", quantity: 2, price: 5.0},
-  {name: "Salad", quantity: 1, price: 4.50},
-  {name: "Coke", quantity: 3, price: 1.50}
+    {name: "Burger", quantity: 2, price: 5.0},
+    {name: "Salad", quantity: 1, price: 4.50},
+    {name: "Coke", quantity: 3, price: 1.50}
 ];
 
-//Initialize binder with data and pass in a callback to run when data is updated.
-// let rootBinder = createBinder(orderData, (newroot) => {
-// 	rootBinder = newroot;
-// });
-
-// rootBinder[0].quantity.setValue(5);
-// rootBinder.push({
-// 	name: 'Fries',
-// 	quantity: 7,
-//   price: 1.00
-// });
-
-// console.log(rootBinder.getValue());
 
 class HelloMessage extends React.Component {
   render() {
@@ -35,9 +22,4 @@ class HelloMessage extends React.Component {
   }
 }
 
-
 ReactDOM.render(<HelloMessage name="Warmbowski" />, document.getElementById("app"));
-
-// orderCortex.onUpdate(function(updatedOrder) {
-//   orderComponent.setProps({order: updatedOrder});
-// });
