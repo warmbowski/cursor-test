de4dc26d2b94b08e0ee26b9ff3f62cb8d6a2079b
Paul Laskowski <paul@warmbowski.org>
2018-11-17 23:58:27 -0800

    rename component file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createBinder } from 'immutable-binder';
 
-import { Order } from "./app";
+import { Order } from "./Order";
 
 const orderData = [
     {name: "Burger", quantity: 2, price: 5.0},
