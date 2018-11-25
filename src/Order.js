2ad8e3e46e501bdf901b9ca8150ede8d4b95222a
Paul Laskowski <paul@warmbowski.org>
2018-11-25 11:29:20 -0800

    fix layout

--- src/Order.js
+++ src/Order.js
@@ -20,17 +20,26 @@ export const Item = ({ item }) => {
     const increase = () => {
         item.quantity.setValue(item.quantity.getValue() + 1);
     }
+    const decrease = () => {
+        const qty = item.quantity.getValue();
+        if (qty > 0) item.quantity.setValue(qty - 1);
+    }
     const subTotal= () => {
         return item.quantity.getValue() * item.price.getValue();
     }
 
 
     return(
-        <div className="item" style={{ width: '150px', display: 'flex', justifyContent: 'space-between' }}>
-            <a href="#" onClick={increase}>+</a>
-            <span>{item.quantity.getValue()}</span>
-            <span>{item.name.getValue()}</span>
-            <span>${subTotal()}</span>
+        <div className="item" style={{ display: 'flex' }}>
+            <div style={{ width: '30px', display: 'flex', justifyContent: 'space-around' }}>
+                <a href="#" onClick={increase}>+</a>
+                <a href="#" onClick={decrease}>-</a>
+            </div>
+            <div style={{ width: '150px', display: 'flex', justifyContent: 'space-between' }}>
+                <span>{item.quantity.getValue()}</span>
+                <span>{item.name.getValue()}</span>
+                <span>${subTotal()}</span>
+            </div>
         </div>
     );
 };
